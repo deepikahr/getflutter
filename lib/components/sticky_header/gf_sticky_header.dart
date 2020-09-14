10be3639be1cd989a76b452cff6cdab67666597a
deepikahr <deepika@pietechsolution.com>
2020-09-14 17:09:35 +0530

    gfstickyheader component created

---
+++ lib/components/sticky_header/gf_sticky_header.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GFStickyHeader extends StatefulWidget {
+  @override
+  _GFStickyHeaderState createState() => _GFStickyHeaderState();
+}
+
+class _GFStickyHeaderState extends State<GFStickyHeader> {
+  @override
+  Widget build(BuildContext context) => Container(
+
+    );
+}
